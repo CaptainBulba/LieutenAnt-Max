45f4b0b6697724e7fb929d72fba4cf160fb971aa
CaptainBulba <privies@mail.ru>
2021-12-19 02:52:01 +0200

    spider mechanics

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Spider : MonoBehaviour
 {
     public Grab grab;
+    public float freezePlayerTime;
+    float randomNumberX;
+    float randomNumberY;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +24,42 @@ public class Spider : MonoBehaviour
         if(grab != null && grab.isPicked)
         {
             grab.itemHolding.GetComponent<Rigidbody2D>().simulated = true;
+            grab.itemHolding.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
             grab.itemHolding.transform.parent = null;
             grab.itemHolding.transform.position = grab.grabDetect.position;
 
-            grab.itemHolding.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 10.0f);
+            float whichSide = Random.Range(0, 2);
+            
+            switch (whichSide)
+            {
+                case 0:
+                    randomNumberX = Random.Range(2.0f, 7.0f);
+                    randomNumberY = Random.Range(2.0f, 7.0f);
+                    break;
+                case 1:
+                    randomNumberX = Random.Range(-2.0f, -7.0f);
+                    randomNumberY = Random.Range(-2.0f, -7.0f);
+                    break;
+            }
+            Debug.Log(whichSide + " RandomX:" + randomNumberX + " RandomY: " + randomNumberY);
+            grab.itemHolding.GetComponent<Rigidbody2D>().velocity = new Vector3(randomNumberX, randomNumberY, 0);
+
             grab.isPicked = false;
+
+            StartCoroutine(FreezePlayer());
         }   
+    }
+    IEnumerator FreezePlayer()
+    {
+        grab.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        grab.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+
+        yield return new WaitForSeconds(freezePlayerTime);
+
+        grab.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        grab.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        grab.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
     }
 }
 
--- Assets/Scripts/Walls.cs
+++ Assets/Scripts/Walls.cs
@@ -7,21 +7,20 @@ public class Walls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Item touched");
+        
         if (collision.collider.tag == "Item")
         {
-            
+         //   Debug.Log("Item touched");
             collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
 
 
